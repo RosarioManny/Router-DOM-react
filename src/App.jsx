50ab9cd476011fc5be2a3cae455b31925561be32
RosarioManny <rosario.emm47@gmail.com>
2024-10-17 12:04:33 -0400

    Navbar and Pokemon Routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,19 @@
 import PokemonList from "./components/PokemonList";
 import { useState } from 'react'
 import InitialState from './InitialState'
+import NavBar from "./components/NavBar";
+import { Route, Routes } from 'react-router-dom'
 
 const App = () => {
   const [pokemon, setPokemon] = useState(InitialState);
   return (
     <>
       <h1>Pokemon!</h1>
-      <PokemonList pokemon={pokemon} />
+      <NavBar />
+      <Routes>
+        <Route path='/pokemon' element={ <PokemonList pokemon={pokemon} />} />
+      </Routes>
+  
     </>
   );
 };
--- src/components/PokemonList.jsx
+++ src/components/PokemonList.jsx
@@ -4,7 +4,7 @@ const PokemonList = (props) => {
         <h2>Pokemon</h2>
         <ul>
           {props.pokemon.map((currentPokemon) => (
-            <li key={currentPokemon.name}>{currentPokemon.name}</li>
+            <li key={currentPokemon._id}>{currentPokemon.name}</li>
           ))}
         </ul>
       </>
